0df83bc44f63dd795d7d6823a740fb080531d463
karan-sahani-dev <karansahani1919@gmail.com>
2026-09-07 15:36:35 +0530

    Day 1: Spring Boot + health endpoint working

---
+++ src/main/java/com/karan/url_shortener/HealthController.java
@@ -0,0 +1,24 @@
+package com.karan.url_shortener;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+
+@RestController
+@RequestMapping("/api")
+public class HealthController {
+    @GetMapping("/health")
+    public ResponseEntity<Map<String, Object>> healthCheck() {
+        Map<String, Object> response = new HashMap<>();
+        response.put("status","UP");
+        response.put("message","URL shortener is running!");
+        response.put("timestamp", LocalDateTime.now().toString());
+        return ResponseEntity.ok(response);
+    }
+}
